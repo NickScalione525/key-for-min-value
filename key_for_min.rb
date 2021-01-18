39e81e88a19399f0408c6ca70137f9ee3ea97628
nick <nicholas_scalione@yahoo.com>
2021-01-18 10:36:30 -0500

    commit tree 1e229d4eba0f3405e067a6c6f8991a266d3e220e

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+    new_array = []
+    name_hash.collect do |name, age|
+        new_array.push age
+    end
 
+    lowest_value = new_array[0]
+    new_array.collect do |i|
+        if i < lowest_value
+        lowest_value = i
+        end
+    end
+
+name_hash.collect do |name, age|
+    if age == lowest_value
+        return name
+    end
+end
+puts nil if name_hash = {}
 end
